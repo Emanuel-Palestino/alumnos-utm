b1c10d49a89765417d29287bf17c61c5d1f57c2b
Emanuel Palestino <emanuel.palestino.h@gmail.com>
2024-04-13 21:49:31 -0600

    [Inicio] Secciones de prueba agregadas.

--- app/routes/app._index.tsx
+++ app/routes/app._index.tsx
@@ -3,8 +3,24 @@ import InformacionEstudiante from "~/components/InformacionEstudiante";
 export default function Inicio() {
 
 	return (
-		<main>
+		<main className="flex gap-8 px-6">
 			<InformacionEstudiante />
+
+			<section className="min-w-80">
+				<h3 className="text-xl font-bold mb-3 text-gray-800">Calendario Escolar</h3>
+
+				<div className="rounded-xl bg-white p-6">
+					En construcción...
+				</div>
+			</section>
+
+			<section className="min-w-80">
+				<h3 className="text-xl font-bold mb-3 text-gray-800">Reporte de Lectura</h3>
+
+				<div className="rounded-xl bg-white p-6">
+					En construcción...
+				</div>
+			</section>
 		</main>
 	)
 
