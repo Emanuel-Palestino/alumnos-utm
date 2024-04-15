7ccdfdce73f4e6aa8387a106179c46700bcbd24e
Emanuel Palestino <emanuel.palestino.h@gmail.com>
2024-04-15 12:38:51 -0600

    Disclaimer agregado en el login.

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -15,7 +15,7 @@ export const meta: MetaFunction = () => {
 export default function Index() {
     return (
         <main className="container m-auto py-12 sm:px-0 px-3">
-            <h1 className="text-2xl font-semibold text-center mb-10">Alumnos UTM</h1>
+            <h1 className="text-3xl font-semibold text-center mb-10">Alumnos UTM</h1>
 
             <section className="rounded-xl bg-white px-8 sm:px-12 sm:py-6 py-4 w-full max-w-[480px] m-auto">
                 <h3 className="text-center text-lg mb-6 font-semibold">Iniciar Sesión</h3>
@@ -26,7 +26,7 @@ export default function Index() {
                         <input
                             id="matricula"
                             className="border border-gray-300 rounded-xl px-4 py-2 outline-gray-500"
-                            type="text"
+                            type="number"
                             placeholder="2000010101"
                         />
                     </div>
@@ -56,13 +56,15 @@ export default function Index() {
                     <small
                         className="text-gray-400 block mt-4 text-center"
                     >
-                        Puedes ingresar con los mismos accesos que utilizas en el <a className="underline" href="https://alumnos.utm.mx/" target="_blank" rel="noreferrer">NES</a>
+                        Debes ingresar con los mismos accesos que utilizas en el <a className="underline" href="https://alumnos.utm.mx/" target="_blank" rel="noreferrer">NES</a>
                     </small>
                 </form>
             </section>
 
             <section className="rounded-xl bg-white px-8 sm:px-12 sm:py-6 py-4 w-full mt-20">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, officiis itaque incidunt ea perferendis non, atque dolor cumque autem, aspernatur alias? Officia dolores possimus impedit porro nulla laboriosam deserunt quis.</p>
+                <p className="mb-3">Esta página es un cliente alternativo y <b>NO OFICIAL</b> diseñado para la página de <strong>estudiantes de la Universidad Tecnológica de la Mixteca</strong>, y no está afiliado, autorizado ni avalado por la misma.</p>
+                <p className="mb-3"> El propósito de este proyecto es ofrecer una <b>experiencia de usuario mejorada</b>, con actualizaciones continuas, una respuesta más rápida y una visualización de datos optimizada.</p>
+                <p>Es importante destacar que este cliente es un proyecto de código abierto creado con fines educativos, por lo que puedes visualizar el código utilizado y <b>NO ALMACENAMOS</b> datos personales en ninguna plataforma externa.</p>
             </section>
         </main>
     )
